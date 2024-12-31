e351d3da025e89d8a8bad6097316e24c673db3c8
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-12-31 22:30:24 +0100

    handle Uncaught Exceptions

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -11,7 +11,7 @@ export default function Error({ error, reset }) {
     return (
         <div className='grid place-content-center min-h-screen'>
             <h2 className='text-red-400 text-2xl font-bold'>Hubo una excepción no capturada</h2>
-            <p className='text-red-400'>¿Has intentado subir una imagen mayor de 4mb?</p>
+            <p className='text-red-700'>¿Has intentado subir una imagen mayor de 4mb?</p>
             <p>{error.message}</p>
             <button className="px-4 py-2 border mt-4 rounded-md text-white bg-blue-500" onClick={() => reset()}>
                 Intentar de nuevo
