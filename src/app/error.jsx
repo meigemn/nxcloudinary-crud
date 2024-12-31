450f288967fb9252a3e6aaf3b17f65ebd8934991
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-12-31 23:07:24 +0100

    Limit of 500 api operations reached

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -14,9 +14,10 @@ export default function Error({ digest, statusCode, error, reset }) {
         <div className='grid place-content-center min-h-screen'>
             <h2 className='text-red-400 text-2xl font-bold'>Hubo una excepción no capturada</h2>
             <p>{error.message}</p>
-            <ul>
-                <li className='text-red-700'>¿Has intentado subir una imagen mayor de 4mb?</li>
-                <li className='text-red-700'>¿Has superado el límite de 500 operaciones?</p>
+
+            <ul className='list-disc p-4'>
+                <li className='text-red-700'>¿Has intentado subir una imagen mayor de 4MB?</li>
+                <li className='text-red-700'>¿Has superado el límite de 500 operaciones?</li>
             </ul>
             <button className="px-4 py-2 border mt-4 rounded-md text-white bg-blue-500" onClick={() => reset()}>
                 Intentar de nuevo
