10a63d233e9c2d0d680959251f55c6663b1af268
José A. Muñoz Jiménez <jamj2000@gmail.com>
2025-01-04 12:41:18 +0100

    simplify

--- src/components/ImageEdit.jsx
+++ src/components/ImageEdit.jsx
@@ -13,19 +13,18 @@ function ImageEdit({ img }) {
     const { refresh } = useRouter()
     const dialogId = useId()
 
-
-    const [stateUpdate, actionUpdate, pendingUpdate] = useActionState(imgUpdate, null)
-    const [stateDelete, actionDelete, pendingDelete] = useActionState(imgDelete, null)
+    const [stateUpdate, actionUpdate, pendingUpdate] = useActionState(imgUpdate, {})
+    const [stateDelete, actionDelete, pendingDelete] = useActionState(imgDelete, {})
 
     useEffect(() => {
-        if (stateUpdate?.success) toast.success(stateUpdate.success)
-        if (stateUpdate?.error) toast.error(stateUpdate.error)
+        stateUpdate.success && toast.success(stateUpdate.success)
+        stateUpdate.error && toast.error(stateUpdate.error)
     }, [stateUpdate])
 
 
     useEffect(() => {
-        if (stateDelete?.success) toast.success(stateDelete.success)
-        if (stateDelete?.error) toast.error(stateDelete.error)
+        stateDelete.success && toast.success(stateDelete.success)
+        stateDelete.error && toast.error(stateDelete.error)
         refresh()     // refrescamos página después de mostrar mensaje de success o error
     }, [stateDelete])
 
@@ -35,10 +34,6 @@ function ImageEdit({ img }) {
 
             <InputImage image={img.secure_url} />
 
-            {/* <span title='VER' onClick={() => { window.open(img.secure_url, "newTab", "popup,left=100,top=100,width=600,height=370") }}
-                className={`absolute bottom-[88px] right-2 p-1 border border-slate-300 bg-blue-400 text-white hover:bg-blue-500 rounded-full`}>
-                <Eye />
-            </span> */}
             <div
                 onClick={() => document.getElementById(dialogId).showModal()}
                 className={`absolute bottom-[88px] right-2 p-1 border border-slate-300 bg-blue-400 text-white hover:bg-blue-500 rounded-full`}>
